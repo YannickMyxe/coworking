0c5f8ca3d39aac2ad1920b27a5ef38bf9d0a5c8d
Kevin <sv.vandendaele.kevin@gmail.com>
2022-04-29 12:17:07 +0200

    fixed eventlistener problem and merge conflicts

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
 ;"use strict";
+(function () {
+ 
 
-let images = ["../img/Dice-1-b.svg.png", "../img/Dice-2-b.svg.png", "../img/Dice-3-b.svg.png", "../img/Dice-4-b.svg.png", "../img/Dice-5-b.svg.png", "../img/Dice-6-b.svg.png"];
+    let images = ["../img/Dice-1-b.svg.png", "../img/Dice-2-b.svg.png", "../img/Dice-3-b.svg.png", "../img/Dice-4-b.svg.png", "../img/Dice-5-b.svg.png", "../img/Dice-6-b.svg.png"];
+    const dice = document.querySelectorAll('.dice-wrapper');
 
-function roll(){
-    return Math.floor(Math.random()*6);
-}
-
-function diceRoll(el){
-    let random = roll()
-    el.setAttribute("src", images[random]);
+    function roll(){
+        return Math.floor(Math.random()*6);
+    }
+    for (let i = 0; i < dice.length; i++) {
+        dice[i].addEventListener("click", function() {
+          diceRoll(dice[i].firstElementChild);
+        });
+    }
+    function diceRoll(el){
+        let random = roll()
+        el.setAttribute("src", images[random]);
+        console.log(getValue(2));
+    }
     
-}
+    function getValue(id){
+        return document.querySelector("#dice-" +id).getAttribute("src").match(/\d+/)[0];
+    }
 
-function getValue(id){
-    return document.querySelector("#dice-" +id).getAttribute("src").match(/\d+/)[0];
-}
+})()
 
-var canvas = document.getElementById("game-canvas");
-var context = canvas.getContext("2d");
+
+
+
+
 
 
 
