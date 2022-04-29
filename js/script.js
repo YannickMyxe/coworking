cb9779f3c6bcb73401f9b4c46c4b301a912f999a
Kevin <sv.vandendaele.kevin@gmail.com>
2022-04-29 10:03:48 +0200

    multiple dices change on click

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 
 let images = ["../img/Dice-1-b.svg.png", "../img/Dice-2-b.svg.png", "../img/Dice-3-b.svg.png", "../img/Dice-4-b.svg.png", "../img/Dice-5-b.svg.png", "../img/Dice-6-b.svg.png"];
 let dice = document.querySelectorAll("img");
-let diceroll;
 function roll(){
     return Math.floor(Math.random()*6);
 }
-diceroll = roll();
-document.querySelector("#dice-1").setAttribute("src", images[diceroll]);
-console.log(diceroll);
+
+function diceroll(el){
+    console.log(el.id);
+    document.querySelector("#" + el.id).setAttribute("src", images[roll()]);
+}
+document.querySelector("#dice-1").setAttribute("src", images[roll()]);
+document.querySelector("#dice-2").setAttribute("src", images[roll()]);
+document.querySelector("#dice-3").setAttribute("src", images[roll()]);
+document.querySelector("#dice-4").setAttribute("src", images[roll()]);
+
 // alert("Dylan was here")
