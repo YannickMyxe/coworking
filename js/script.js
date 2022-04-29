18f8c678a552cae0195ff6caacb4c110547e89d5
Kevin <sv.vandendaele.kevin@gmail.com>
2022-04-29 10:07:23 +0200

    clean up unnecesairy code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 ;"use strict";
 
 let images = ["../img/Dice-1-b.svg.png", "../img/Dice-2-b.svg.png", "../img/Dice-3-b.svg.png", "../img/Dice-4-b.svg.png", "../img/Dice-5-b.svg.png", "../img/Dice-6-b.svg.png"];
-let dice = document.querySelectorAll("img");
+
 function roll(){
     return Math.floor(Math.random()*6);
 }
@@ -10,9 +10,5 @@ function diceroll(el){
     console.log(el.id);
     document.querySelector("#" + el.id).setAttribute("src", images[roll()]);
 }
-document.querySelector("#dice-1").setAttribute("src", images[roll()]);
-document.querySelector("#dice-2").setAttribute("src", images[roll()]);
-document.querySelector("#dice-3").setAttribute("src", images[roll()]);
-document.querySelector("#dice-4").setAttribute("src", images[roll()]);
 
 // alert("Dylan was here")
