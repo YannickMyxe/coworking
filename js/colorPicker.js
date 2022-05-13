83dc4b20b157093a92861dac9a98dfa119743687
YannickMyxe <shawn.froste.sf@gmail.com>
2022-05-13 11:29:58 +0200

    color picker finished

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -4,6 +4,7 @@
     const form = document.querySelector("#color-picker");
     const picker = document.querySelector("#color");
     const button = document.querySelector("#color-button");
+    const playerSelector = document.querySelector(`#speler`);
 
     form.addEventListener('submit', e => {
         e.preventDefault();
@@ -11,7 +12,16 @@
 
     button.addEventListener('click', () => {
         const color = picker.value;
-        console.log(color);
-        document.querySelector(':root').style.setProperty('--primary', color); 
+        const player = playerSelector.value;
+
+        let property;
+        switch(player) {
+           case "player-1":  property = '--green-color'; break;
+           case "player-2":  property = '--yellow-color'; break;
+           case "player-3":  property = '--blue-color'; break;
+           case "player-4":  property = '--red-color'; break;
+        }
+
+        document.querySelector(':root').style.setProperty(property, color); 
     });
 }) ();
