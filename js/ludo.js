02ba98d776d1109d46e24eefd93abf6b7c2357a5
Dylan Daelman <dylan.daelman@telenet.be>
2022-06-03 12:30:47 +0200

    dice

--- js/ludo.js
+++ js/ludo.js
@@ -59,8 +59,6 @@ const pawnSvg = (index) => {
     marianne.setAttribute("d", "M44,53v-1.843c0-2.274-1.837-4.123-4.104-4.152L39.562,44H20.438l-0.334,3.005C17.837,47.034,16,48.883,16,51.157V53h-2v7 h32v-7H44z")
     dylan.appendChild(marianne)
     console.log(dylan);
-    
-
 
     return dylan;
 };
@@ -69,10 +67,56 @@ const pawnSvg = (index) => {
 
 
 const pawn_space = document.querySelector(`.pawn-space`);
-pawn_space.appendChild(pawnSvg(1))
+// pawn_space.appendChild(pawnSvg(1))
 
 // for (let index = 0; index < (aantalPionen * aantalSpelers); index++) {
 //     pawn_space.appendChild( pawnSvg(index % aantalSpelers) );
 // };
 
+let g1 = pawn_space.children[0];
+let g2 = pawn_space.children[1];
+let g3 = pawn_space.children[2];
+let g4 = pawn_space.children[3];
 
+let y1 = pawn_space.children[4];
+let y2 = pawn_space.children[5];
+let y3 = pawn_space.children[6];
+let y4 = pawn_space.children[7];
+
+let b1 = pawn_space.children[8];
+let b2 = pawn_space.children[9];
+let b3 = pawn_space.children[10];
+let b4 = pawn_space.children[11];
+
+let r1 = pawn_space.children[12];
+let r2 = pawn_space.children[13];
+let r3 = pawn_space.children[14];
+let r4 = pawn_space.children[15];
+
+// g1.square.getBoundingClientRect().x = 150;
+console.log(`®へぅぴ　山手くださいますようお願い申し上`);
+
+
+let dice = document.querySelector(".dice");
+let roll = document.querySelector(".roll");
+dice.addEventListener('click', () => {
+    let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+    if(diceRoll === 1) {
+        roll.innerHTML = ("⚀");
+    } else if(diceRoll === 2) {
+        roll.innerHTML = ("⚁");
+    }
+    else if(diceRoll === 3) {
+        roll.innerHTML = ("⚂");
+    }
+    else if(diceRoll === 4) {
+        roll.innerHTML = ("⚃");
+    }
+    else if(diceRoll === 5) {
+        roll.innerHTML = ("⚄");
+    }
+    else if(diceRoll === 6) {
+        roll.innerHTML = ("⚅");
+    }
+})
